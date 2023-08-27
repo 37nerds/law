7bc60782352721e5c9afb3a675462610b072c23d
Shihab Mahamud <shihab4t@gmail.com>
2023-08-27 21:08:17 +0600

    Redis queue working fine

--- api/config/database.php
+++ api/config/database.php
@@ -72,7 +72,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
